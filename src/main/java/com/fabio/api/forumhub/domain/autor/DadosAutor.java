5f44a3d3374de783a9e67fc66b752aac459c2964
DevFabJava <dev.fabioalmeida@proton.me>
2025-07-21 19:25:37 -0300

    Primeiro commit

---
+++ src/main/java/com/fabio/api/forumhub/domain/autor/DadosAutor.java
@@ -0,0 +1,7 @@
+package com.fabio.api.forumhub.domain.autor;
+
+public record DadosAutor(Long id, String nome, String email) {
+
+    public DadosAutor(Autor autor){
+        this(autor.getId(), autor.getNome(), autor.getEmail());}
+}
