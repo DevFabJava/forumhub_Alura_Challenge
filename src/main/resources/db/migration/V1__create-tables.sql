63483b274c56d6de24c48371f813fdb5683e20dc
DevFabJava <dev.fabioalmeida@proton.me>
2025-07-28 20:09:06 -0300

    Atualizando migration e application.properties

---
+++ src/main/resources/db/migration/V1__create-tables.sql
@@ -0,0 +1,37 @@
+CREATE TABLE autores (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE
+);
+
+CREATE TABLE cursos (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    categoria ENUM('mobile', 'programação', 'front-end',
+     'devops', 'ux & design', 'data science',
+     'inovação & gestão', 'inteligência artificial') NOT NULL
+);
+
+CREATE TABLE topicos (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    titulo VARCHAR(255) NOT NULL,
+    mensagem TEXT NOT NULL,
+    data_criacao DATETIME NOT NULL,
+    status VARCHAR(50) NOT NULL,
+    autor_id BIGINT NOT NULL,
+    curso_id BIGINT NOT NULL,
+
+    FOREIGN KEY (autor_id) REFERENCES autores(id),
+    FOREIGN KEY (curso_id) REFERENCES cursos(id)
+);
+
+CREATE TABLE respostas (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    solucao TEXT,
+    topico_id BIGINT NOT NULL,
+    data_criacao DATETIME NOT NULL,
+    autor_id BIGINT NOT NULL,
+
+    FOREIGN KEY (topico_id) REFERENCES topicos(id),
+    FOREIGN KEY (autor_id) REFERENCES autores(id)
+);
